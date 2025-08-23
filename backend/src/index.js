34ad4571911f010f9841e377821cf8b6f50ed29a
Anns Qasim <anns.qasim@gmail.com>
2025-08-23 14:03:19 +0500

    feat: - CRUD for properties (update, delete, archive) - Added viewing model, controller, and routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import connectDB from "../config/db.js";
 
 import propertyRoutes from "./routes/propertyRoutes.js";
 import clientRoutes from "./routes/clientRoutes.js";
+import viewingRoutes from "./routes/viewingRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use("/api/properties", propertyRoutes);
 app.use("/api/clients", clientRoutes);
+app.use("/api/viewings", viewingRoutes);
 
 app.get("/api/test", (req, res) => {
     res.json({ message: "test route works" });
